aa677cb0f5579c1525091df170d94a99565bfa8a
Tai Kedzierski <dch.tai@gmail.com>
2023-09-20 14:36:16 +0100

    Add lenkerx

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 main() {
     cargo build --release > build-release.log 2>&1
     mkdir -p ~/.local/bin
-    cp target/release/lenker ~/.local/bin/lenker
+    cp target/release/lenker lenkerx/lenkerx ~/.local/bin/
 
     if ! which lenker >/dev/null; then
         echo "Add ~/.local/bin to your PATH"
